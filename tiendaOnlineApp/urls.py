0db1a960ff6af96a55119dea66e1b8cd99ce1f99
Sergio Santiago <sergio.santiago@insinno.es>
2024-01-11 08:41:41 +0100

    gitignore

--- tiendaOnlineApp/urls.py
+++ tiendaOnlineApp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -23,4 +25,8 @@ urlpatterns = [
     path("shop/", views.shop, name="shop"),
     path("blog/", views.blog, name="blog"),
     path("contact/", views.contact, name="contact"),
+    
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tiendaOnlineApp/views.py
+++ tiendaOnlineApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from services.models import Service
 
 # Create your views here.
 
@@ -6,7 +7,8 @@ def home(request):
     return render(request, 'tiendaOnlineApp/home.html')
 
 def services(request):
-    return render(request, 'tiendaOnlineApp/services.html')
+    services = Service.objects.all()
+    return render(request, 'tiendaOnlineApp/services.html', {'services': services})
 
 def shop(request):
     return render(request, 'tiendaOnlineApp/shop.html')
